6f1c3d862b59013a8c671d9ba1681b6e144933f4
Adolfo Neto <adolfo.usp@gmail.com>
2022-03-17 09:30:56 -0300

    Todos os conectivos da logica classica proposicional foram implementados

--- logica/lib/logica.ex
+++ logica/lib/logica.ex
@@ -1,18 +1,36 @@
 defmodule Logica do
+  @doc """
+  Função que recebe dois argumentos que são valores lógicos (ou seja,
+  só podem ser :verdadeiro ou :falso) e retorna a conjunção lógica ("e") dos valores recebidos.
+  """
+  @spec e(:falso | :verdadeiro, :falso | :verdadeiro) :: :falso | :verdadeiro
+  def e(:verdadeiro, :verdadeiro), do: :verdadeiro
+  def e(:falso, :verdadeiro), do: :falso
+  def e(:verdadeiro, :falso), do: :falso
+  def e(:falso, :falso), do: :falso
 
-  def e(:verdadeiro,:verdadeiro) do
+  def ou(:falso, :falso) do
+    :falso
+  end
+
+  def ou(:verdadeiro, :falso) do
     :verdadeiro
   end
-  def e(_,_) do
-    :falso
+
+  def ou(:falso, :verdadeiro) do
+    :verdadeiro
   end
-  def ou(:falso,:falso) do
-    :falso
-  end
-  def ou(_,_) do
+
+  def ou(:verdadeiro, :verdadeiro) do
     :verdadeiro
   end
 
   def nao(:verdadeiro), do: :falso
   def nao(:falso), do: :verdadeiro
+
+  @spec implica(:falso | :verdadeiro, :falso | :verdadeiro) :: :falso | :verdadeiro
+  def implica(:verdadeiro, :verdadeiro), do: :verdadeiro
+  def implica(:falso, :verdadeiro), do: :verdadeiro
+  def implica(:verdadeiro, :falso), do: :falso
+  def implica(:falso, :falso), do: :verdadeiro
 end
